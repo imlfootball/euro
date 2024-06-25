c5775a850853feda4de3ab720b7743236e2b91f8
Olivier Lebrasse <olivier.infomil2019@gmail.com>
2024-06-25 20:29:24 +0400

    Bracket Game WIP

--- src/app/pages/games/bracket/bracket.component.ts
+++ src/app/pages/games/bracket/bracket.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class BracketComponent {
 
+  winnerR161: string = '-';
+  winnerR162: string = '-';
+  winnerR163: string = '-';
+  winnerR164: string = '-';
+  winnerR165: string = '-';
+  winnerR166: string = '-';
+  winnerR167: string = '-';
+  winnerR168: string = '-';
+
+  winnerR41: string = '-';
+  winnerR42: string = '-';
+  winnerR43: string = '-';
+  winnerR44: string = '-';
+
+  winnerSemi1: string = '-';
+  winnerSemi2: string = '-';
+
+  winnerEuro: string = '-';
+
+  selectedWinner(phase: string, selected: Event):void {
+    console.log(phase, selected.target);
+  }
 }
